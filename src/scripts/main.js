edbe8427016f12222beea5d72574f0c702a63e57
CommunalWheat9 <46871372+CommunalWheat9@users.noreply.github.com>
2022-04-30 10:02:37 +0200

    Popup clean

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,18 @@
 let buyButton = document.querySelector("#buyButton");
+let buyButtonExitPay = document.querySelector("#popUpExitPay");
 let divPopUp = document.querySelector(".payPopUp");
 let divPopUpClose = document.querySelector(".PopUpButton");
+
+
 
 if(buyButton){
     buyButton.addEventListener('click', event => {
         divPopUp.classList.add("open");
     });
     divPopUpClose.addEventListener('click', event => {
+        divPopUp.classList.remove("open");
+    });
+    buyButtonExitPay.addEventListener('click', event => {
         divPopUp.classList.remove("open");
     });
 
